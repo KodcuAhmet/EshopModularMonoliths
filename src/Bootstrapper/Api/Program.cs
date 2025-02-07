36cab1142f0d5be8f78f1b97bc622564c5216daa
KodcuAhmet <ahmet.cigil@tuta.com>
2025-02-07 15:34:46 +0100

    40. Develop AddCatalogModule extention method in CatalogModule.cs

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -7,6 +7,10 @@ namespace Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container
+            builder.Services
+                .AddCatalogModule(builder.Configuration)
+                .AddBasketModule(builder.Configuration)
+                .AddOrderingModule(builder.Configuration);
 
             var app = builder.Build();
 
